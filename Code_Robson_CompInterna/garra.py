d13096700c60e2de48bef1a36d0a73cd0bd6d817
David Fanchic Chatelard <davidfchatelard@gmail.com>
2021-09-22 21:27:18 -0300

    Funções de pegar e soltar cubo funcionando

--- Code_Robson_CompInterna/garra.py
+++ Code_Robson_CompInterna/garra.py
@@ -5,7 +5,6 @@ import simConst
 
 ## FUNÇÕES DA GARRA ######################################
 
-#Nas funções da garra, vamos ter que definir quando quisermos manipular uma das duas garras, pois como agora vamos ter duas, isso vai ter que ser especificado em cada função de mover a garra
 
 #essas funções de subir e descer a garra estavam sem a parte de pauseCommunication, não sei se tem problema
 def subir_garra_frente(object, altura):
@@ -69,84 +68,58 @@ def disable_magic_cube(object, cubo):
 
 def fechar_garra_frente_cubo(object, cube):
     erro = 1
-    # while erro != 0:
-    #     erro, robotPosition = sim.simxGetObjectPosition(object.clientID, object.robot, -1, sim.simx_opmode_streaming)
-    # erro = 1
     while erro != 0:
         erro, cubePosition = sim.simxGetObjectPosition(object.clientID, cube, object.robot, sim.simx_opmode_streaming)
-    #print(robotPosition)
-    # print('Antes: ', cubePosition)
-    cubePosition[0] = -0.02
-    cubePosition[1] = 0.02
-    cubePosition[2] = 0.03
-    # print('Depois: ', cubePosition)
+    cubePosition[0] = 0.04 #se aumentar, vai mais para baixo
+    cubePosition[1] = 0 #se aumentar, vai mais para dentro do robô
+    cubePosition[2] = 0 #se aumentar, vai mais para a esquerda
 
     erro = 1
-    while erro != 0: #verificar se essa parte ta certa
+    while erro != 0:
         sim.simxPauseCommunication(object.clientID, True)
-        sim.simxSetJointTargetPosition(object.clientID,object.paDireita1,0.02,sim.simx_opmode_oneshot) 
-        sim.simxSetJointTargetPosition(object.clientID,object.paEsquerda1,-0.02,sim.simx_opmode_oneshot)
-        erro = sim.simxSetObjectPosition(object.clientID, cube, object.garraEsq, cubePosition, sim.simx_opmode_oneshot)
-        sim.simxSetObjectParent(object.clientID, cube, object.garraEsq, True, sim.simx_opmode_oneshot)
-        sim.simxSetObjectIntParameter(object.clientID, cube, 3003, 1, sim.simx_opmode_oneshot)
+        sim.simxSetObjectIntParameter(object.clientID, cube, 3003, 1, sim.simx_opmode_oneshot)# torna o cubo estático
+        # sim.simxSetJointTargetPosition(object.clientID,object.pa_direita1,0.2,sim.simx_opmode_oneshot) #se pa não precisa fechar a garra
+        # sim.simxSetJointTargetPosition(object.clientID,object.pa_esquerda1,0.2,sim.simx_opmode_oneshot)
+        erro = sim.simxSetObjectPosition(object.clientID, cube, object.pa_esquerda1, cubePosition, sim.simx_opmode_oneshot)
+        sim.simxSetObjectParent(object.clientID, cube, object.cubo_acoplador2, True, sim.simx_opmode_oneshot)
         sim.simxPauseCommunication(object.clientID, False)
     time.sleep(1)
 
 def abrir_garra_frente_cubo(object, cube):
-    # erro = 1
-    # while erro != 0:
-    #     erro, cubePosition = sim.simxGetObjectPosition(object.clientID, cube, -1, sim.simx_opmode_streaming)
-    # # cubePosition[2] = 0
-    # erro = 1
-    # while erro != 0: #verificar se essa parte ta certa
     sim.simxPauseCommunication(object.clientID, True)
-    sim.simxSetJointTargetPosition(object.clientID, object.paDireita1, 0, sim.simx_opmode_oneshot) #ver essas distâncias pra deixar a garra aberta
-    sim.simxSetJointTargetPosition(object.clientID, object.paEsquerda1, 0, sim.simx_opmode_oneshot) #ver essas distâncias pra deixar a garra aberta
-    # erro = sim.simxSetObjectPosition(object.clientID, cube, object.robot, cubePosition, sim.simx_opmode_oneshot)#talvez de pra tirar essa linha e tirar o while
+    sim.simxSetJointTargetPosition(object.clientID, object.pa_direita1, 0.005, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetPosition(object.clientID, object.pa_esquerda1, 0.005, sim.simx_opmode_oneshot)
     sim.simxSetObjectParent(object.clientID, cube, -1, True, sim.simx_opmode_oneshot)
-    sim.simxSetObjectIntParameter(object.clientID, cube, 3003, 0, sim.simx_opmode_oneshot)
+    sim.simxSetObjectIntParameter(object.clientID, cube, 3004, 1, sim.simx_opmode_oneshot) # torna o cubo respondable
+    sim.simxSetObjectIntParameter(object.clientID, cube, 3003, 0, sim.simx_opmode_oneshot) # torna o cubo dinâmico
     sim.simxPauseCommunication(object.clientID, False)
-        #erro = 0
     time.sleep(1)
 
 def fechar_garra_costas_cubo(object, cube):
     erro = 1
-    # while erro != 0:
-    #     erro, robotPosition = sim.simxGetObjectPosition(object.clientID, object.robot, -1, sim.simx_opmode_streaming)
-    # erro = 1
     while erro != 0:
         erro, cubePosition = sim.simxGetObjectPosition(object.clientID, cube, object.robot, sim.simx_opmode_streaming)
-    #print(robotPosition)
-    # print('Antes: ', cubePosition)
-    cubePosition[0] = -0.02
-    cubePosition[1] = 0.02
-    cubePosition[2] = 0.03
-    # print('Depois: ', cubePosition)
+    cubePosition[0] = 0.04 #se aumentar, vai mais para baixo
+    cubePosition[1] = 0 #se aumentar, vai mais para dentro do robô
+    cubePosition[2] = 0 #se aumentar, vai mais para a esquerda
 
     erro = 1
-    while erro != 0: #verificar se essa parte ta certa
+    while erro != 0:
         sim.simxPauseCommunication(object.clientID, True)
-        sim.simxSetJointTargetPosition(object.clientID,object.paDireita2,0.02,sim.simx_opmode_oneshot) 
-        sim.simxSetJointTargetPosition(object.clientID,object.paEsquerda2,-0.02,sim.simx_opmode_oneshot)
-        erro = sim.simxSetObjectPosition(object.clientID, cube, object.garraEsq, cubePosition, sim.simx_opmode_oneshot)
-        sim.simxSetObjectParent(object.clientID, cube, object.garraEsq, True, sim.simx_opmode_oneshot)
-        sim.simxSetObjectIntParameter(object.clientID, cube, 3003, 1, sim.simx_opmode_oneshot)
+        sim.simxSetObjectIntParameter(object.clientID, cube, 3003, 1, sim.simx_opmode_oneshot)# torna o cubo estático
+        # sim.simxSetJointTargetPosition(object.clientID,object.pa_direita2,0.2,sim.simx_opmode_oneshot) #se pa não precisa fechar a garra
+        # sim.simxSetJointTargetPosition(object.clientID,object.pa_esquerda2,0.2,sim.simx_opmode_oneshot)
+        erro = sim.simxSetObjectPosition(object.clientID, cube, object.pa_esquerda2, cubePosition, sim.simx_opmode_oneshot)
+        sim.simxSetObjectParent(object.clientID, cube, object.cubo_acoplador2, True, sim.simx_opmode_oneshot)
         sim.simxPauseCommunication(object.clientID, False)
     time.sleep(1)
 
 def abrir_garra_costas_cubo(object, cube):
-    # erro = 1
-    # while erro != 0:
-    #     erro, cubePosition = sim.simxGetObjectPosition(object.clientID, cube, -1, sim.simx_opmode_streaming)
-    # # cubePosition[2] = 0
-    # erro = 1
-    # while erro != 0: #verificar se essa parte ta certa
     sim.simxPauseCommunication(object.clientID, True)
-    sim.simxSetJointTargetPosition(object.clientID, object.paDireita2, 0, sim.simx_opmode_oneshot) #ver essas distâncias pra deixar a garra aberta
-    sim.simxSetJointTargetPosition(object.clientID, object.paEsquerda2, 0, sim.simx_opmode_oneshot) #ver essas distâncias pra deixar a garra aberta
-    # erro = sim.simxSetObjectPosition(object.clientID, cube, object.robot, cubePosition, sim.simx_opmode_oneshot)#talvez de pra tirar essa linha e tirar o while
+    sim.simxSetJointTargetPosition(object.clientID, object.pa_direita2, 0.005, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetPosition(object.clientID, object.pa_esquerda2, 0.005, sim.simx_opmode_oneshot)
     sim.simxSetObjectParent(object.clientID, cube, -1, True, sim.simx_opmode_oneshot)
-    sim.simxSetObjectIntParameter(object.clientID, cube, 3003, 0, sim.simx_opmode_oneshot)
+    sim.simxSetObjectIntParameter(object.clientID, cube, 3004, 1, sim.simx_opmode_oneshot) # torna o cubo respondable
+    sim.simxSetObjectIntParameter(object.clientID, cube, 3003, 0, sim.simx_opmode_oneshot) # torna o cubo dinâmico
     sim.simxPauseCommunication(object.clientID, False)
-        #erro = 0
     time.sleep(1)
