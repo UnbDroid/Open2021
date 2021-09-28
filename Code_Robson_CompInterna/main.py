d107c58a268d4e90219fc841fc878ca5a7e6ab46
Arthur <arthurcontroleambiental@gmail.com>
2021-09-28 16:44:26 -0300

    codigo de alinhar, para evitar que ele saia do quadrado

--- Code_Robson_CompInterna/main.py
+++ Code_Robson_CompInterna/main.py
@@ -23,14 +23,46 @@ global clientID
 clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
 robotname = 'S_Base'
 
-if clientID != -1:
 
+if clientID != -1:
     sim.simxStartSimulation(clientID, sim.simx_opmode_oneshot_wait)
     print('Connected to remote API server')
     sim.simxAddStatusbarMessage(clientID, 'Funcionando...', sim.simx_opmode_oneshot_wait)
     time.sleep(0.02)
-
     adeni = ObjectHandle(clientID, robotname) #instancia objeto
+    girando = 0
+    cor_esquerda = Ler_Cor(adeni ,'esquerda')
+    cor_direita = Ler_Cor(adeni, 'direita')
+
+    def alinhar():
+        while True:
+            esquerda = Ler_Cor(adeni, 'esquerda')
+            direita = Ler_Cor(adeni, 'direita')
+            if esquerda =='PRETO' or direita =='preto':
+                print('quebrei')
+                break
+            else:
+                print('to andando')
+                move_frente(adeni,3)
+        while True:
+            flag = 0
+            corE =Ler_Cor(adeni, 'esquerda')
+            corD = Ler_Cor(adeni, 'direita')
+            if flag:
+                break
+            while Ler_Cor(adeni, 'esquerda')== 'PRETO' and Ler_Cor(adeni, 'direita') == 'BRANCO':
+                print('cor esquerda PRETO')
+                giro_livre(adeni, 1,1)
+                flag =1
+            while Ler_Cor(adeni, 'esquerda') == 'BRANCO' and Ler_Cor(adeni, 'direita') == 'PRETO':
+                print('cor direita PRETA')
+                giro_livre(adeni, -1,1)
+                flag = 1
+            else:
+                move_frente(adeni, 3)
+    while True:
+        alinhar()
+        
     # while True:
     # turn_around_angle(adeni, 100, 1, 2)
     # girar_90_graus(adeni, 1)
@@ -39,7 +71,9 @@ if clientID != -1:
 
     # giro_livre(adeni,1,2)
     # andar_em_metros(adeni, 'tras', 2, 1)
-
+    #while girando<1:
+     #   alinhar
+    
 
 
 else:
