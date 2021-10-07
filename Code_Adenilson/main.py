9e1b8c901be354bdde3ad883cdbbfb163226196a
welder2 <weldersilva326@gmail.com>
2021-10-07 01:18:14 -0300

    Upgrade

--- Code_Adenilson/main.py
+++ Code_Adenilson/main.py
@@ -23,30 +23,6 @@ except:
     print('')
 
 
-def firstAreaCubes(currentPosition, myDirection, order):
-    if(order == 1):
-        destine = 22
-        direction = LESTE
-        lastTurn = direita
-    if(order == 2):
-        destine = 23
-        direction = OESTE
-        lastTurn = esquerda
-    #Vai para a primeira área
-    currentPosition, myDirection = deA_para_B(adeni, currentPosition, destine, myDirection)
-    #Se posiciona da melhor forma para enxergar os blocos
-    myDirection = turnTo(adeni, myDirection ,direction, True)
-    #Align() #TurnTo ja alinha
-    andar_em_metros(adeni,frente, 2, 0.04)
-    girar_90_graus(adeni,lastTurn)
-    myDirection = SUL
-    #align.Align()
-    andar_em_metros(adeni, tras, 5, 0.065)
-    matrix0 = vis.resolveVision(adeni, 0)
-    #time.sleep(3)
-    return matrix0, currentPosition, myDirection
-
-
 sim.simxFinish(-1)  # just in case, close all opened connections
 global clientID
 clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
@@ -59,25 +35,7 @@ if clientID != -1:
     sim.simxAddStatusbarMessage(clientID, 'Funcionando...', sim.simx_opmode_oneshot_wait)
     time.sleep(0.02)
     adeni = ObjectHandle(clientID, robotname) #instancia objeto
-    initialDirection = SUL
-    print(sim.simxGetObjectHandle(clientID, 'Camera_Superior_Vision', sim.simx_opmode_blocking))
-    initialPosition = firstSq.identifyFirstPos(adeni)
-    if(initialPosition[1] == -1):
-        andar_em_metros(frente, 6, 0.20)
-        # align.Align()
-        move.andar_em_metros(tras, 5, 0.065)
-    iniY, iniX = firstSq.identifyFirstPos(adeni)
-    initialPosition = (iniY+1)*10+(iniX+1)
-    firstAreaCubes(initialPosition, initialDirection, 1) #vai quebrar não está pronto
-    # IndoDeA_para_B(adeni,31,34,SUL,SUL)
-    # IndoDeA_para_B(adeni,64,61,SUL,SUL)
-    IndoDeA_para_B(adeni,27,21,SUL,SUL)
-    # entregandoCubos(adeni,22,23,SUL,NORTE)
-    # IndoDeA_para_B(adeni,47,22,OESTE,NORTE)
-    # IndoDeA_para_B(adeni,61,21,SUL,SUL)
-    # IndoDeA_para_B(adeni,21,27,SUL,LESTE)
-
-
+    
 
 
 else:
