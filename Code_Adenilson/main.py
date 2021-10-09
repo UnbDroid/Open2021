c18799657a8ffcd933e6d42ac49be76a79e5637d
welder2 <weldersilva326@gmail.com>
2021-10-09 01:32:59 -0300

    Casos Especiais e Entrega de Blocos

--- Code_Adenilson/main.py
+++ Code_Adenilson/main.py
@@ -36,7 +36,8 @@ if clientID != -1:
     time.sleep(0.02)
     adeni = ObjectHandle(clientID, robotname) #instancia objeto
     # IndoDeA_para_B(adeni,24,27,SUL,NORTE)
-    print(firstSq.identifyFirstPos(adeni))
+    # poze = firstSq.identifyFirstPos(adeni)
+    # print(melhorbloco(poze,))
 
 
 else:
